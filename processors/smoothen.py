c5840ec339a388192a31356818161bd2ac548cc7
Algeron Waterschoot <hello@algeron.be>
2022-12-28 15:08:20 +0100

    Added TODO info

--- processors/smoothen.py
+++ processors/smoothen.py
@@ -1,6 +1,9 @@
 import pymeshlab
 import sys
 
+PASSES = 6  # TODO: Higher passes create dimples in the model which continue to grow deeper. Find out how to avoid that.
+# TODO: Explore alternative smoothing algorithms. An interesting candidate is the "HC" variant of Laplacian smoothing, but this creates triangular holes in the mesh with every pass, and the "fix holes" / "watertight" functions weirdly don't seem to do anything to address this...
+
 
 def smoothen(mesh_filepath):
 
@@ -9,7 +12,7 @@ def smoothen(mesh_filepath):
     mesh.load_new_mesh(mesh_filepath)
 
     # Apply filter
-    mesh.apply_coord_laplacian_smoothing(stepsmoothnum=6)
+    mesh.apply_coord_laplacian_smoothing(stepsmoothnum=PASSES)
     # mesh.compact_faces()  # TODO: Was removed in latest pymeshlab but is still in meshlab. Find out if this feature still exists under a new function name.
     # mesh.compact_vertices()
 
